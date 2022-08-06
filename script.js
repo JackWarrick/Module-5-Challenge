e0f729856cbfe487ace555176377b446e4963099
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2022-08-05 20:01:10 -0500

    most recent

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 var today = moment()
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-var currentHour = moment().format("HH")
-$("#currentHour").text(currentHour);
+var currentHour = moment().format("HH");
 
 
 //write a function for each table row (10 of them) that uses the moment function to determine if it is before that hour, during the hour, or the hour is still coming - if (time is past 8:00){make background color gray with .past css class} else if (time is between 8:00 and 9:00){make background color red with .present css class} else {make background green with .future css class because it is the only other explanation}
@@ -92,11 +91,29 @@ if (currentHour >= 18){
 
 //Now write a function that allows us to set text content for the agenda items - and also have some text there before we start typing that is gray like 'enter agenda item here'
 
+//I am getting the value of the input now and I am able to alert it - i need to take that value and append it to the html as an html element and clear the input box again
 
-var userInput8 = $("#8-item").val();
+//Now I can add list items under the text input - i need to take each of those list items and save them to local storage using the same function!
 
-$("#8-item-save").click(function () {
+//CLEAR INPUT BOX IF WE CAN - figure that out after local storage
+
+//I've stored the last user input but not every one that is entered - maybe we can ask this
+
+
+$("#8-item-save").on("click", function () {
     var userInput8 = $("#8-item").val();
-    alert(userInput8);
-    $("#8-item").append(userInput8);
+    var userInput8Print = $("<li>");
+    userInput8Print.text(userInput8);
+    userInput8Print.addClass("my-2");
+    $("#8-item-container").append(userInput8Print);
+    localStorage.setItem("8-agenda-item", userInput8);
 });
+
+
+/* // get the text
+var text = $('#test').text();
+
+// set the item in localStorage
+localStorage.setItem('test', text)
+
+*/
